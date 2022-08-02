0a18425f2f013ff807988a9e46b35c40dd34ad8b
Kamyab Nazari <kamyab.nz@gmail.com>
2022-08-02 12:28:17 +0200

    Manually moved folders

--- Scripts/BackButton.gd
+++
@@ -1,8 +0,0 @@
-# Author Kamyab Nazari - 2022
-
-extends Panel
-
-onready var sound_Click = GameAudio.get_node("Effect/Click")
-
-func backAction():
-	sound_Click.play()
--- Scripts/PlayersList.gd
+++
@@ -1,8 +0,0 @@
-# Author Kamyab Nazari - 2022
-
-extends Node2D
-
-func move(target):
-	var move_tween = get_node("Players/move_tween")
-	move_tween.interpolate_property(self, "position", position, target, Tween.TRANS_QUINT, Tween.EASE_OUT)
-	move_tween.start()
